2f1bde2ec2b61af9fc7af170aec21e3f7616a06b
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-27 01:49:30 +0530

    Create testbench.v

---
+++ Prob_11/testbench.v
@@ -0,0 +1,34 @@
+module seq_detector_tb;
+reg x;
+reg clk;
+reg reset;
+wire z;
+seq_110011_moore uut (
+    .x(x), 
+    .clk(clk), 
+    .reset(reset), 
+    .z(z)
+);
+
+always #5 clk = ~ clk;  
+
+initial begin
+ $dumpfile("dump.vcd");
+ $dumpvars(1, testbench);
+ end
+
+    initial
+    begin
+    clk = 1'b0;
+    reset = 1'b1;
+    #15 reset = 1'b0;
+    begin 
+    #12 x = 0;#10 x = 0 ; #10 x = 1 ; #10 x = 1 ;
+    #12 x = 0;#10 x = 0 ; #10 x = 1 ; #10 x = 1 ;
+    #12 x = 1;#10 x = 0 ; #10 x = 0 ; #10 x = 1 ;
+    #12 x = 0;#10 x = 1 ; #10 x = 1 ; #10 x = 0 ;
+    #10 $finish;
+    end
+     
+end 
+endmodule
