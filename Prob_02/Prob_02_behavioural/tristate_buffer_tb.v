11d2b77c08400af6cdff825b237a9d977bcefa2d
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-27 10:10:02 +0530

    Update tristate_buffer_tb.v

--- Prob_02/Prob_02_behavioural/tristate_buffer_tb.v
+++ Prob_02/Prob_02_behavioural/tristate_buffer_tb.v
@@ -1,12 +1,12 @@
 module tri_buffer_tb;
 	reg d;
-  reg en;
+        reg enable;
 	wire y;
-	tri_buffer uut(d,en,y);
+	tri_buffer uut(d,enable,y);
 	initial
 	begin
                 #5
-		en = 1;
+		enable = 1;
 		d = 1; # 20; 
 		if ( y == 0 ) 
 			$display ("Pass %d %d",d,y);
