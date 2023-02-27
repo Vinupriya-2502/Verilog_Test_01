8f38e1446a7ec16f44e88e44badb31395b6e16c7
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-27 14:56:49 +0530

    Update testbench.v

--- Prob_09/testbench.v
+++ Prob_09/testbench.v
@@ -2,11 +2,14 @@ module sync_fifo_tb;
   wire [31:0] DATAOUT;
   wire full, empty;
   reg clock, reset, wn, rn;
-  reg [31:0] DATAIN; 
-  sync_fifo dut(DATAOUT, full, empty, clock, reset, wn, rn, DATAIN);
+  reg [31:0] DATAIN;  
+  
+  sync_fifo dut( clock, reset, wn, rn, DATAIN,DATAOUT, full, empty);
+  
   initial begin 
     $dumpfile("fifo_wave.vcd"); $dumpvars;
   end
+
   initial
   begin
     clock = 0; DATAIN = 8'd0;
@@ -27,6 +30,7 @@ module sync_fifo_tb;
     clock = 1; #5 ; clock = 0; #5;
     DATAIN = 8'd45;
     clock = 1; #5 ; clock = 0; #5;
+   
     wn = 0; rn = 1;
     clock = 1; #5 ; clock = 0; #5;
     clock = 1; #5 ; clock = 0; #5;
@@ -36,5 +40,7 @@ module sync_fifo_tb;
     clock = 1; #5 ; clock = 0; #5;
     clock = 1; #5 ; clock = 0; #5;
     clock = 1; #5 ; clock = 0; #5;
+   
   end
+
 endmodule
